1e5a0f9dad444cc025b75b559e138391446109bd
Home <home@AGM-606503.local>
2022-04-28 09:03:01 -0300

    Section Massage and Price

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,23 +3,23 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: '',
     loadChildren:() => import('./modules/home/home.module').then((m) => m.HomeModule)
   },
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: 'home'
+    redirectTo: ''
   },
   {
     path: '**',
-    redirectTo: 'home',
+    redirectTo: '',
     pathMatch: 'full'
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
