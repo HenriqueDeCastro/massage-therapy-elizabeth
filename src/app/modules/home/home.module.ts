eb407f71d781d0bfb6733910ca2fe1c2bf333fd2
Home <home@AGM-606503.local>
2022-04-29 23:49:05 -0300

    remove prices and new benefits

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -5,14 +5,12 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { CardInfoModule } from 'src/app/modules/home/components/card-info/card-info.module';
-import { CardPriceModule } from './components/card-price/card-price.module';
 
 @NgModule({
   declarations: [ HomeComponent ],
   imports: [
     CommonModule,
     CardInfoModule,
-    CardPriceModule,
     ButtonSchedulingModule,
     HomeRoutingModule
   ]
